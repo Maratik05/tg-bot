bae61aa4e203eaf543904a843bd1acf2ff47d66b
Maratik05 <149616737+Maratik05@users.noreply.github.com>
2025-10-04 16:59:44 +0300

    added command help and list

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/Maratik05/tg-bot/internal/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Not found file .env")
+	}
 	token := os.Getenv("TOKEN")
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
@@ -26,13 +31,42 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
+		if update.Message == nil { // If we got a message
+			continue
 		}
+		service := product.NewService()
+		fmt.Print("nan")
+		switch update.Message.Command() {
+		case "help":
+			helpCommand(bot, update.Message)
+		case "list":
+			listCommand(bot, update.Message, service)
+		default:
+			defaultResponse(bot, update.Message)
+		}
+
 	}
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "/help - help")
+	bot.Send(msg)
+}
+
+func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *product.Service) {
+	products := productService.List()
+	outputMsg := "List products\n\n"
+
+	for _, product := range products {
+		outputMsg += product.Title
+		outputMsg += "\n"
+	}
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMsg)
+	bot.Send(msg)
+}
+func defaultResponse(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, inputMessage.Text)
+	msg.ReplyToMessageID = inputMessage.MessageID
+	bot.Send(msg)
 }
---
+++ internal/product/models.go
@@ -0,0 +1,13 @@
+package product
+
+var AllProducts = []Product{
+	{Title: "One"},
+	{Title: "Two"},
+	{Title: "Three"},
+	{Title: "four"},
+	{Title: "Five"},
+}
+
+type Product struct {
+	Title string
+}
